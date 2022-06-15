a50840c82df9ab5d6a49281a9289a3a83f7dc724
玛卡巴卡 <3281531833@qq.com>
2022-06-15 10:54:23 +0800

    addon kether launch

---
+++ src/main/kotlin/com/bh/planners/core/kether/enhance/ActionLaunch.kt
@@ -0,0 +1,48 @@
+package com.bh.planners.core.kether.enhance
+
+import com.bh.planners.core.kether.NAMESPACE
+import com.bh.planners.core.kether.createTargets
+import org.bukkit.util.Vector
+import taboolib.common5.Coerce
+import taboolib.library.kether.ArgTypes
+import taboolib.library.kether.ParsedAction
+import taboolib.module.kether.*
+import java.util.concurrent.CompletableFuture
+
+class ActionLaunch(
+    val x: ParsedAction<*>,
+    val y: ParsedAction<*>,
+    val z: ParsedAction<*>,
+    val selector: ParsedAction<*>
+) : ScriptAction<Void>() {
+
+    override fun run(frame: ScriptFrame): CompletableFuture<Void> {
+        frame.newFrame(x).run<Any>().thenApply { x ->
+            frame.newFrame(y).run<Any>().thenApply { y ->
+                frame.newFrame(z).run<Any>().thenApply { z ->
+                    frame.createTargets(selector).thenApply { container ->
+                        container.forEachEntity {
+                            val vector1 = this.location.direction.setY(0).normalize()
+                            val vector2 = vector1.clone().crossProduct(Vector(0, 1, 0))
+                            vector1.multiply(Coerce.toDouble(x))
+                            vector1.add(vector2.multiply(Coerce.toDouble(z))).y = Coerce.toDouble(y)
+                            this.velocity = vector1
+                        }
+                    }
+                }
+            }
+        }
+        return CompletableFuture.completedFuture(null)
+    }
+
+    internal object Parser {
+
+        @KetherParser(["launch"], namespace = NAMESPACE)
+        fun parser() = scriptParser {
+            val x = it.next(ArgTypes.ACTION)
+            val y = it.next(ArgTypes.ACTION)
+            val z = it.next(ArgTypes.ACTION)
+            ActionLaunch(x, y, z, it.next(ArgTypes.ACTION))
+        }
+    }
+}
