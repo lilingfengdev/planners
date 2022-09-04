a67c8c02365285736e4b662e1b7299888848cae4
玛卡巴卡 <3281531833@qq.com>
2022-09-04 20:27:18 +0800

    addon

--- src/main/kotlin/com/bh/planners/core/kether/game/ActionGlowing.kt
+++ src/main/kotlin/com/bh/planners/core/kether/game/ActionGlowing.kt
@@ -2,7 +2,11 @@ package com.bh.planners.core.kether.game
 
 import com.bh.planners.api.common.SimpleTimeoutTask
 import com.bh.planners.core.kether.*
+import com.bh.planners.core.kether.util.GlowUtil
+import org.bukkit.ChatColor
 import org.bukkit.entity.Entity
+import taboolib.common.platform.function.info
+import taboolib.common5.Coerce
 import taboolib.library.kether.ArgTypes
 import taboolib.library.kether.ParsedAction
 import taboolib.module.kether.KetherParser
@@ -11,14 +15,14 @@ import taboolib.module.kether.ScriptFrame
 import taboolib.module.kether.scriptParser
 import java.util.concurrent.CompletableFuture
 
-class ActionGlowing(val tick: ParsedAction<*>, val value: ParsedAction<*>, val selector: ParsedAction<*>?) :
+class ActionGlowing(val tick: ParsedAction<*>, val value: ParsedAction<*>, val color: ParsedAction<*>, val selector: ParsedAction<*>?) :
     ScriptAction<Void>() {
 
-    fun execute(entity: Entity, value: Boolean, tick: Long) {
-        entity.isGlowing = value
+    fun execute(entity: Entity, value: Boolean, color: ChatColor, tick: Long) {
+        GlowUtil.setColor(entity, color)
         if (tick == -1L) return
         SimpleTimeoutTask.createSimpleTask(tick, true) {
-            entity.isGlowing = !value
+            GlowUtil.removeColor(entity)
         }
     }
 
@@ -26,12 +30,15 @@ class ActionGlowing(val tick: ParsedAction<*>, val value: ParsedAction<*>, val s
 
         frame.runTransfer0<Long>(tick) { tick ->
             frame.runTransfer0<Boolean>(value) { value ->
-                if (selector != null) {
-                    frame.execEntity(selector) {
-                        execute(this, value, tick)
+                frame.runTransfer0<ChatColor>(color) { color ->
+                    val glowColor = Coerce.toEnum(color, ChatColor::class.java)
+                    if (selector != null) {
+                        frame.execEntity(selector) {
+                            execute(this, value, glowColor, tick)
+                        }
+                    } else {
+                        execute(frame.asPlayer() ?: return@runTransfer0, value, glowColor, tick)
                     }
-                } else {
-                    execute(frame.asPlayer() ?: return@runTransfer0, value, tick)
                 }
             }
         }
@@ -44,12 +51,14 @@ class ActionGlowing(val tick: ParsedAction<*>, val value: ParsedAction<*>, val s
         /**
          * 设置目标发光，-1为永久值 需要取消
          * glowing <timeout: action(-1)> <value: action(true)> <selector>
+         * glowing <timeout> <value> <color> <selector>
          */
         @KetherParser(["glowing"], namespace = NAMESPACE, shared = true)
         fun parser() = scriptParser {
             ActionGlowing(
                 it.tryGet(arrayOf("tick", "time", "timeout"), -1)!!,
                 it.tryGet(arrayOf("value"), true)!!,
+                it.tryGet(arrayOf("color"), ChatColor.WHITE)!!,
                 it.selectorAction()
             )
         }
