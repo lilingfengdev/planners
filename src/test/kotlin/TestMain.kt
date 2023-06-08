999253e59b9ae303d0002cc3e95729ad6b3a98e7
纸杯 <785740487@qq.com>
2023-06-08 16:15:40 +0000

    重新上传项目
    Signed-off-by: 纸杯 <785740487@qq.com>

---
+++ src/test/kotlin/TestMain.kt
@@ -0,0 +1,10 @@
+object TestMain {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        SelectorTransfer(":@ttt ab 1 4 5 @ass 45ds sa").run().thenAccept {
+            println("===")
+        }
+    }
+
+}
