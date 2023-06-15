26a86e842be8850cc98286c820475056a12721e3
zhibei <785740487@qq.com>
2023-06-15 18:04:56 +0800

    fix

--- src/main/kotlin/com/bh/planners/core/kether/game/ActionSilence.kt
+++ src/main/kotlin/com/bh/planners/core/kether/game/ActionSilence.kt
@@ -4,33 +4,27 @@ import com.bh.planners.api.event.PlayerCastSkillEvents
 import com.bh.planners.api.event.PlayerSilenceEvent
 import com.bh.planners.core.kether.NAMESPACE
 import com.bh.planners.core.kether.execPlayer
-import com.bh.planners.core.kether.nextArgumentAction
 import com.bh.planners.core.kether.nextSelectorOrNull
 import taboolib.common.platform.event.EventPriority
 import taboolib.common.platform.event.SubscribeEvent
 import taboolib.common5.Coerce
 import taboolib.library.kether.ParsedAction
-import taboolib.module.kether.KetherParser
-import taboolib.module.kether.ScriptAction
-import taboolib.module.kether.ScriptFrame
-import taboolib.module.kether.scriptParser
+import taboolib.module.kether.*
 import java.util.*
 import java.util.concurrent.CompletableFuture
 
 class ActionSilence(
-    val callevent: ParsedAction<*>,
+    val callevent: Boolean,
     val ticks: ParsedAction<*>,
     val selector: ParsedAction<*>?,
 ) : ScriptAction<Void>() {
 
     override fun run(frame: ScriptFrame): CompletableFuture<Void> {
-        frame.newFrame(callevent).run<Boolean>().thenAccept { callevent ->
-            frame.newFrame(ticks).run<Any>().thenApply { ticks ->
-                frame.execPlayer(selector!!) {
-                    silenceMap[uniqueId] = (System.currentTimeMillis() + Coerce.toDouble(ticks) * 50).toLong()
-                    if (callevent) {
-                        PlayerSilenceEvent(this, (Coerce.toDouble(ticks) * 50).toLong()).call()
-                    }
+        frame.newFrame(ticks).run<Any>().thenApply { ticks ->
+            frame.execPlayer(selector!!) {
+                silenceMap[uniqueId] = (System.currentTimeMillis() + Coerce.toDouble(ticks) * 50).toLong()
+                if (callevent) {
+                    PlayerSilenceEvent(this, (Coerce.toDouble(ticks) * 50).toLong()).call()
                 }
             }
         }
@@ -44,13 +38,18 @@ class ActionSilence(
         /**
          * 沉默目标 使对方在一定时间内无法释放技能
          *  *** 暂时无效 等PlayerCastSkillEvents完善即可
-         * silence <callevent: false> [ticks] [selector]
+         * silence <callevent> [ticks] [selector]
          */
         @KetherParser(["silence"], namespace = NAMESPACE, shared = true)
         fun parser() = scriptParser {
-            val callevent = it.nextArgumentAction(arrayOf("callevent"), "false")!!
-            val ticks = it.nextParsedAction()
-            ActionSilence(callevent, ticks, it.nextSelectorOrNull())
+            it.switch {
+                case("callevent") {
+                    ActionSilence(true, it.nextParsedAction(), it.nextSelectorOrNull())
+                }
+                other {
+                    ActionSilence(false, it.nextParsedAction(), it.nextSelectorOrNull())
+                }
+            }
         }
 
         @SubscribeEvent(EventPriority.LOWEST)
