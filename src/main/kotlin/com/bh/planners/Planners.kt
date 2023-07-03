018719a8c49850ea753624fc7f36910b8584c606
zhibei <785740487@qq.com>
2023-07-04 02:26:16 +0800

    new 统计

--- src/main/kotlin/com/bh/planners/Planners.kt
+++ src/main/kotlin/com/bh/planners/Planners.kt
@@ -1,8 +1,11 @@
 package com.bh.planners
 
+import org.bukkit.Bukkit
 import taboolib.common.env.RuntimeDependency
 import taboolib.common.platform.Platform
 import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.adaptCommandSender
+import taboolib.common5.Mirror
 import taboolib.module.configuration.Config
 import taboolib.module.configuration.Configuration
 import taboolib.module.metrics.Metrics
@@ -20,5 +23,9 @@ object Planners : Plugin() {
 
     override fun onEnable() {
         Metrics(15573, BukkitPlugin.getInstance().description.version, Platform.BUKKIT)
+    }
+
+    override fun onDisable() {
+        Mirror.report(adaptCommandSender(Bukkit.getServer().consoleSender))
     }
 }
