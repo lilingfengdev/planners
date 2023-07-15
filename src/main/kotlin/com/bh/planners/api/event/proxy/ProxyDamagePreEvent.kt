b4d8bc684d2cc27c0bd14eb888918af640236d01
zhibei <785740487@qq.com>
2023-07-15 16:42:48 +0800

    fix

--- src/main/kotlin/com/bh/planners/api/event/proxy/ProxyDamagePreEvent.kt
+++ src/main/kotlin/com/bh/planners/api/event/proxy/ProxyDamagePreEvent.kt
@@ -1,14 +1,15 @@
 package com.bh.planners.api.event.proxy
 
 import ac.github.oa.internal.base.enums.PriorityEnum
+import com.bh.planners.core.kether.game.damage.DamageType
 import org.bukkit.entity.Entity
 import org.bukkit.event.entity.EntityDamageEvent
 import taboolib.common.platform.event.EventPriority
 import taboolib.common.platform.event.OptionalEvent
 import taboolib.common.platform.event.SubscribeEvent
 
-class ProxyDamagePreEvent(damager: Entity, entity: Entity, cause: EntityDamageEvent.DamageCause?, damage: Double) :
-    AbstractProxyDamageEvent(damager, entity, cause, damage) {
+class ProxyDamagePreEvent(damager: Entity, entity: Entity, cause: EntityDamageEvent.DamageCause?, damage: Double, type: DamageType) :
+    AbstractProxyDamageEvent(damager, entity, cause, damage, type) {
 
     companion object {
 
@@ -24,7 +25,7 @@ class ProxyDamagePreEvent(damager: Entity, entity: Entity, cause: EntityDamageEv
                 val memory = e.damageMemory
                 val damager = e.damageMemory.event.damager
                 val damageEvent =
-                    ProxyDamagePreEvent(damager, memory.injured, memory.event.bukkitCause, memory.totalDamage)
+                    ProxyDamagePreEvent(damager, memory.injured, memory.event.bukkitCause, memory.totalDamage, DamageType.PHYSICS)
                 damageEvent.data["@OriginAttribute:Memory"] = memory
                 damageEvent.event = e.damageMemory.event.origin
                 damageEvent.call()
