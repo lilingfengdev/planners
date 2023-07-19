5cde1b7a81326adb238da9a0b0b7b236790bbdae
zhibei <785740487@qq.com>
2023-07-19 14:24:35 +0800

    fix

--- src/main/kotlin/com/bh/planners/core/selector/bukkit/Target.kt
+++ src/main/kotlin/com/bh/planners/core/selector/bukkit/Target.kt
@@ -1,10 +1,11 @@
 package com.bh.planners.core.selector.bukkit
 
 import com.bh.planners.core.effect.Target.Companion.getLivingEntity
-import com.bh.planners.core.effect.Target.Companion.ifEntity
+import com.bh.planners.core.effect.Target.Companion.getPlayer
 import com.bh.planners.core.effect.Target.Companion.toTarget
 import com.bh.planners.core.selector.Selector
 import org.bukkit.Material
+import org.bukkit.entity.Entity
 import taboolib.common.platform.function.submit
 import java.util.concurrent.CompletableFuture
 
@@ -21,35 +22,39 @@ object Target : Selector {
         get() = arrayOf("target", "t")
 
     override fun check(data: Selector.Data): CompletableFuture<Void> {
+
         val range = data.read<Int>(0, "1")
         val blocked = data.read<Boolean>(1, "false")
         val point = data.read<Boolean>(2, "false")
-        val future = CompletableFuture<Void>()
-        data.origin.ifEntity {
-            submit(async = false) {
-                val blocks = if (point) {
-                    listOf(data.origin.getLivingEntity()?.getTargetBlock(if (blocked) setOf(Material.AIR) else allMaterials, range) ?: return@submit)
-                } else {
-                    data.origin.getLivingEntity()?.getLineOfSight(if (blocked) setOf(Material.AIR) else allMaterials, range) ?: return@submit
-                }
-                blocks.forEach {
-                    it.world.getNearbyEntities(it.location, 1.0, 1.0, 1.0).forEach { entity ->
-                        data.container += entity.toTarget()
-                    }
-                }
-            }
-        }
-        future.complete(null)
-        return future
-    }
 
-    private val allMaterials: Set<Material>
-        get() {
-            val materials: MutableSet<Material> = mutableSetOf()
-            for (material in Material.values()) {
-                materials.add(material)
+        val future = CompletableFuture<Void>()
+
+        submit(async = false) {
+            val blocks = if (point) {
+                listOf(
+                    data.origin.getLivingEntity()
+                        ?.getTargetBlock(if (blocked) setOf(Material.AIR) else Material.values().map { it }.toSet(), range) ?: return@submit
+                )
+            } else {
+                data.origin.getLivingEntity()?.getLineOfSight(if (blocked) setOf(Material.AIR) else Material.values().map { it }.toSet(), range)
+                    ?: return@submit
             }
-            return materials 
+            val entitys = mutableSetOf<Entity>()
+            blocks.forEach {
+                it.world.getNearbyEntities(it.location, 1.0, 1.0, 1.0).forEach { entity ->
+                    entitys.add(entity)
+                }
+            }
+            entitys.forEach{
+                data.container += it.toTarget()
+                data.origin.getPlayer()?.sendMessage(it.toString() + it.name)
+            }
         }
+
+        future.complete(null)
+
+        return future
+
+    }
 
 }
