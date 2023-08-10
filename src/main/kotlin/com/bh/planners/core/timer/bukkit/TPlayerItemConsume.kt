59b870daff6882e42959171ec58768f82178b0ca
KunSs <wen20020419>
2023-08-11 00:00:35 +0800

    fix ItemStack name

--- src/main/kotlin/com/bh/planners/core/timer/bukkit/TPlayerItemConsume.kt
+++ src/main/kotlin/com/bh/planners/core/timer/bukkit/TPlayerItemConsume.kt
@@ -6,6 +6,7 @@ import com.bh.planners.core.timer.AbstractTimer
 import com.bh.planners.core.timer.Template
 import org.bukkit.event.player.PlayerItemConsumeEvent
 import taboolib.module.kether.ScriptContext
+import taboolib.module.nms.getName
 
 object TPlayerItemConsume : AbstractTimer<PlayerItemConsumeEvent>() {
     override val name: String
@@ -27,7 +28,7 @@ object TPlayerItemConsume : AbstractTimer<PlayerItemConsumeEvent>() {
     override fun onStart(context: ScriptContext, template: Template, e: PlayerItemConsumeEvent) {
         super.onStart(context, template, e)
         val item = e.item
-        context.rootFrame().variables()["displayName"] = item.itemMeta?.displayName
+        context.rootFrame().variables()["displayName"] = item.getName()
         context.rootFrame().variables()["lore"] = item.itemMeta?.lore
         context.rootFrame().variables()["material"] = item.type.toString()
         context.rootFrame().variables()["item"] = item
