680bd5c54bd573ff10cb957a9ddb7bead1957837
Jie <1503745098@qq.com>
2023-08-11 00:02:55 +0800

    修改创建bossbar的id

--- src/main/kotlin/com/bh/planners/core/kether/game/bossbar/ActionBossBarCreate.kt
+++ src/main/kotlin/com/bh/planners/core/kether/game/bossbar/ActionBossBarCreate.kt
@@ -1,20 +1,18 @@
 package com.bh.planners.core.kether.game.bossbar
 
-import com.bh.planners.util.generatorId
 import taboolib.library.kether.ParsedAction
-import taboolib.module.kether.ScriptAction
-import taboolib.module.kether.ScriptFrame
-import taboolib.module.kether.long
-import taboolib.module.kether.run
+import taboolib.module.kether.*
 import java.util.concurrent.CompletableFuture
 
-class ActionBossBarCreate(val tick: ParsedAction<*>) : ScriptAction<String>() {
+class ActionBossBarCreate(val id: ParsedAction<*>, val tick: ParsedAction<*>) : ScriptAction<String>() {
 
     override fun run(frame: ScriptFrame): CompletableFuture<String> {
         val future = CompletableFuture<String>()
-        frame.run(tick).long { tick ->
-            val bossbar = BossBarManager.createBossbar(generatorId().toString(), tick)
-            future.complete(bossbar.id)
+        frame.run(id).str { id ->
+            frame.run(tick).long { tick ->
+                val bossbar = BossBarManager.createBossbar(id, tick)
+                future.complete(bossbar.id)
+            }
         }
         return future
     }
