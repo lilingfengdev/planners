b9a3c8d2f6ca5998c15bb6219dc9ba04a15dd4c4
AmazingOcean <2191746730@qq.com>
2023-08-14 11:25:06 +0800

    fix

--- src/main/kotlin/com/bh/planners/core/kether/game/ActionVelocity.kt
+++ src/main/kotlin/com/bh/planners/core/kether/game/ActionVelocity.kt
@@ -1,9 +1,15 @@
 package com.bh.planners.core.kether.game
 
+import com.bh.planners.core.effect.Target
 import com.bh.planners.core.kether.NAMESPACE
+import com.bh.planners.core.kether.common.CombinationKetherParser
+import com.bh.planners.core.kether.common.KetherHelper
+import com.bh.planners.core.kether.common.KetherHelper.containerOrSender
+import com.bh.planners.core.kether.common.MultipleKetherParser
 import com.bh.planners.core.kether.containerOrSender
 import com.bh.planners.core.kether.nextSelectorOrNull
 import org.bukkit.entity.Entity
+import org.bukkit.entity.LivingEntity
 import org.bukkit.util.Vector
 import taboolib.common5.Coerce
 import taboolib.library.kether.ParsedAction
@@ -11,65 +17,54 @@ import taboolib.module.kether.*
 import taboolib.module.navigation.set
 import java.util.concurrent.CompletableFuture
 
-class ActionVelocity(
-    val mode: Mode,
-    val x: ParsedAction<*>,
-    val y: ParsedAction<*>,
-    val z: ParsedAction<*>,
-    val selector: ParsedAction<*>?,
-) : ScriptAction<Void>() {
+@CombinationKetherParser.Used
+object ActionVelocity : MultipleKetherParser() {
 
-    enum class Mode {
-        ADD, SUBTRACT, MULTIPLY, DIVIDE, SET
-    }
-
-    fun execute(entity: Entity, mode: Mode, vector: Vector) {
-        val vec = entity.velocity
-        when (mode) {
-            Mode.ADD -> vec.add(vector)
-            Mode.SUBTRACT -> vec.subtract(vector)
-            Mode.MULTIPLY -> vec.multiply(vector)
-            Mode.DIVIDE -> vec.divide(vector)
-            Mode.SET -> vec.set(vector.x, vector.y, vector.z)
-        }
-        entity.velocity = vec
-    }
-
-    override fun run(frame: ScriptFrame): CompletableFuture<Void> {
-        frame.newFrame(x).run<Any>().thenApply { x ->
-            frame.newFrame(y).run<Any>().thenApply { y ->
-                frame.newFrame(z).run<Any>().thenApply { z ->
-                    val toVector = Vector(Coerce.toDouble(x), Coerce.toDouble(y), Coerce.toDouble(z))
-                    frame.containerOrSender(selector).thenAccept {
-                        it.forEachEntity { execute(this, mode, toVector) }
-                    }
-                }
+    @CombinationKetherParser.Ignore
+    fun actionParser(block: ScriptFrame.(Vector,Target.Container) -> Unit) = KetherHelper.simpleKetherParser<Unit>() {
+        it.group(double(), double(), double(),containerOrSender()).apply(it) { x,y,z,container ->
+            now {
+                block(this, Vector(x, y, z),container)
             }
         }
-        return CompletableFuture.completedFuture(null)
     }
 
-    internal object Parser {
-
-        /**
-         * 为目标设置 "固定" 向量 (不跟随视角方向)
-         * velocity [mode] [x] [y] [z] [selector]
-         * velocity add 1 1 0 they "@self"
-         */
-        @KetherParser(["velocity"], namespace = NAMESPACE, shared = true)
-        fun parser() = scriptParser {
-            val mode = when (it.expects("add", "subtract", "sub", "minus", "multiply", "mul", "div", "divide", "set")) {
-                "add" -> Mode.ADD
-                "subtract", "sub", "minus" -> Mode.SUBTRACT
-                "multiply", "mul" -> Mode.MULTIPLY
-                "divide", "div" -> Mode.DIVIDE
-                "set" -> Mode.SET
-                else -> error("error")
-            }
-            val x = it.nextParsedAction()
-            val y = it.nextParsedAction()
-            val z = it.nextParsedAction()
-            ActionVelocity(mode, x, y, z, it.nextSelectorOrNull())
+    fun add() = actionParser { vector, container ->
+        container.forEachLivingEntity {
+            generateVelocity { add(vector) }
         }
     }
+
+    fun subtract() = actionParser { vector, container ->
+        container.forEachLivingEntity {
+            generateVelocity { subtract(vector) }
+        }
+    }
+
+    fun multiply() = actionParser { vector, container ->
+        container.forEachLivingEntity {
+            generateVelocity { multiply(vector) }
+        }
+    }
+
+    fun divide() = actionParser { vector, container ->
+        container.forEachLivingEntity {
+            generateVelocity { divide(vector) }
+        }
+    }
+
+    fun set() = actionParser { vector, container ->
+        container.forEachLivingEntity {
+            generateVelocity {
+                this.x = vector.x
+                this.y = vector.y
+                this.z = vector.z
+            }
+        }
+    }
+
+    fun LivingEntity.generateVelocity(block: Vector.() -> Unit) {
+        this.velocity = velocity.apply(block)
+    }
+
 }
