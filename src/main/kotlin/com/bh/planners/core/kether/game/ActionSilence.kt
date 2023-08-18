6e42a843c3077e3dd7222cc8ab038be6c2457ece
KunSs <wen20020419>
2023-08-18 22:45:54 +0800

    fix ActionSilence

--- src/main/kotlin/com/bh/planners/core/kether/game/ActionSilence.kt
+++ src/main/kotlin/com/bh/planners/core/kether/game/ActionSilence.kt
@@ -2,6 +2,7 @@ package com.bh.planners.core.kether.game
 
 import com.bh.planners.api.event.PlayerCastSkillEvents
 import com.bh.planners.api.event.PlayerSilenceEvent
+import com.bh.planners.core.kether.common.CombinationKetherParser
 import com.bh.planners.core.kether.common.KetherHelper.containerOrSender
 import com.bh.planners.core.kether.common.SimpleKetherParser
 import taboolib.common.platform.event.EventPriority
@@ -10,6 +11,7 @@ import taboolib.module.kether.*
 import java.util.*
 import kotlin.collections.set
 
+@CombinationKetherParser.Used
 object ActionSilence : SimpleKetherParser("silence") {
 
     override fun run(): ScriptActionParser<out Any?> {
